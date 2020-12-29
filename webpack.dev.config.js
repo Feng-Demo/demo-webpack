20293e24c7bbe20aa03986b006b54afe4fe8fd2e
fengfeng <fengfeng@ebnew.com>
2020-12-29 18:03:23 +0800

    引入html-webpack-plugin插件，生成html文件，注意版本号

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 //webpack ./main.js  ./build.js
-const path = require('path');
+const path = require('path');   //处理路径
+const htmlWebpackPlugin = require('html-webpack-plugin');   //生成html文件
 module.exports = {
     // entry: './main.js',
     entry: {
@@ -7,7 +8,7 @@ module.exports = {
     },
     output: {
         path:path.resolve(__dirname,'./dist'),  //打包文件输出目录
-        publicPath:'./dist/',   //打包之后的文件需要配置访问路径
+        //publicPath:'./dist/',   //打包之后的文件需要配置访问路径
         filename: 'build.js'    //打包完成输出文件名称
     },
     watch:true,     //监听代码改动，需要刷新页面
@@ -22,5 +23,10 @@ module.exports = {
                 loader: 'url-loader?limit=133,404'
             }
         ]
-    }
+    },
+    plugins:[
+        new htmlWebpackPlugin({ //生成html文件
+            template: './index.html'
+        })
+    ]
 };
